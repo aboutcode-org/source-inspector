02984c4b19ebfdc614cd9bbdab7d115d276b511c
Ayan Sinha Mahapatra <ayansmahapatra@gmail.com>
2025-08-01 02:23:44 +0530

    Add tests for more C++ source sybol extraction
    Reference: https://github.com/aboutcode-org/source-inspector/issues/41
    Signed-off-by: Ayan Sinha Mahapatra <ayansmahapatra@gmail.com>

---
+++ tests/data/symbols_tree_sitter/cpp/ThreadBuffer.hpp
@@ -0,0 +1,45 @@
+#pragma once
+#include <mutex>
+#include <queue>
+
+template <typename T>
+class FrameBuffer : public std::queue<T>
+{
+public:
+    unsigned int frameCount;
+    FrameBuffer() : frameCount(0) { mutex = new std::mutex(); };
+
+    void pushFrame(const T &FrameBuffering)
+    {
+        std::lock_guard<std::mutex> lock(*mutex);
+
+        std::queue<T>::push(FrameBuffering);
+        frameCount += 1;
+    }
+
+    T getFrame()
+    {
+        std::lock_guard<std::mutex> lock(*mutex);
+        T lastestFrame = this->front();
+        this->pop();
+        frameCount -= 1;
+        return lastestFrame;
+    }
+
+    T peekFrame()
+    {
+        std::lock_guard<std::mutex> lock(*mutex);
+        T lastestFrame = this->front();
+        return lastestFrame;
+    }
+
+    void clearBuffer()
+    {
+        std::lock_guard<std::mutex> lock(*mutex);
+        while (!this->empty())
+            this->pop();
+    }
+
+private:
+    std::mutex *mutex;
+};
